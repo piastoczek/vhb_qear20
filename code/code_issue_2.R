9f10817ef7b6a8f010aba12cfad51aeecd0f933d
piastoczek <55142557+piastoczek@users.noreply.github.com>
2020-08-26 16:09:52 +0200

    Update code_issue_2.R

--- code/code_issue_2.R
+++ code/code_issue_2.R
@@ -25,13 +25,17 @@ library(tidyverse)
 
 # Load datasets
 orbis_data <- read_csv("F:/empirical_accounting_vhb/vhb_qear20/raw_data/orbis_wrds_de.csv")
-
 insolvency_data <- read_csv("F:/empirical_accounting_vhb/vhb_qear20/raw_data/insolvency_filings_de_julaug2020_incomplete.csv")
 
+#Overview
+table(insolvency_data$date) #data from 01/07/2020 to 18/08/2020
+table(insolvency_data$subject) # 10 different factors
+table(insolvency_data$insolvency_court)
+dplyr::count(insolvency_data, insolvency_court, sort = TRUE) #176 insolvency courts
 
-
-
-
+#Basic bar charts
+a <- ggplot(data = insolvency_filings) + geom_bar(mapping = aes(x = subject, fill = subject)) 
+a + theme(axis.text.x = element_blank())
 
 
 
