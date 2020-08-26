5919e7b40ca7488d44217c2e12d9d279ad33300c
piastoczek <55142557+piastoczek@users.noreply.github.com>
2020-08-26 10:27:28 +0200

    Create code_issue_2.R

---
+++ code/code_issue_2.R
@@ -0,0 +1,41 @@
+# ------------------------------------------------------------------------------
+# VHB Course - Group 5
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Issue 2 - Solutions
+# ------------------------------------------------------------------------------
+
+# Set working directories
+
+# Pia 
+# setwd("F:/empirical_accounting_vhb/vhb_qear20")
+
+# Simone
+# setwd("C:/Users/Simone/git/vhb_qear20/raw_data")
+
+# Olga
+#
+ 
+# Tim
+#
+
+#Load Packages
+library(tidyverse)
+
+
+# Load datasets
+orbis_raw <- read_csv("F:/empirical_accounting_vhb/vhb_qear20/raw_data/orbis_wrds_de.csv")
+
+insolvency_filings <- read_csv("F:/empirical_accounting_vhb/vhb_qear20/raw_data/insolvency_filings_de_julaug2020_incomplete.csv")
+
+
+
+
+
+
+
+
+
+
+
+
