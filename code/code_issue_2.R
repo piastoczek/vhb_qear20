d2314f79ec473cda9670039a2889fa36c82e2c58
piastoczek <55142557+piastoczek@users.noreply.github.com>
2020-08-31 09:55:53 +0200

    Update code_issue_2.R

--- code/code_issue_2.R
+++ code/code_issue_2.R
@@ -19,9 +19,13 @@
 # Tim
 #
 
+# Packages if not installed
+#install.packages("summarytools")
+
 #Load Packages
 library(tidyverse)
-
+library(ggplot2)
+library(summarytools)
 
 # Load datasets
 orbis_data <- read_csv("raw_data/orbis_wrds_de.csv")
@@ -33,19 +37,35 @@ table(insolvency_data$subject) # 10 different factors
 table(insolvency_data$insolvency_court)
 dplyr::count(insolvency_data, insolvency_court, sort = TRUE) #176 insolvency courts
 
-#Frequency tables
-#install.packages("summarytools")
-library(summarytools)
-summarytools::freq(insolvency_data$insolvency_court, order = "freq")
-summarytools::freq(insolvency_data$subject, order = "freq")
+#Frequency tables (no option to create pdf with summarytools)
+summary_table_1 <- summarytools::freq(insolvency_data$insolvency_court, order = "freq")
+view(summary_table_1, file = "~/summary_table_1.md")
+summary_table_2 <- summarytools::freq(insolvency_data$subject, order = "freq")
+view(summary_table_2, file = "~/summary_table_2.rmd")
 
 #Basic bar charts
-a <- ggplot(data = insolvency_data) + geom_bar(mapping = aes(x = subject, fill = subject)) 
-a + theme(axis.text.x = element_blank())
+# Bar chart 1
+pdf("output/insolvency_subject_barchart1.pdf")
+insolvency_subject_bar1 <- ggplot(data = insolvency_data) + geom_bar(mapping = aes(x = subject, fill = subject)) 
+insolvency_subject_bar1 + theme(axis.text.x = element_blank())
+print(insolvency_subject_bar1)
+dev.off()
 
+# Bar chart 2
+pdf("output/insolvency_subject_barchart2.pdf")
+insolvency_subject_bar2 <- ggplot(insolvency_data) + geom_bar(mapping = aes(x = subject)) + coord_flip()
+insolvency_subject_bar2 + theme(axis.text.x = element_blank())
+print(insolvency_subject_bar2)
+dev.off()
 
-
-
+# Bar chart 3
+pdf("output/insolvency_subject_barchart3.pdf")
+insolvency_subject_bar3 <- insolvency_data %>% group_by(subject) %>% count() %>% arrange(desc(n))
+ggplot(data = insolvency_subject_bar3) + 
+  geom_col(mapping = aes(x = n, y = reorder(subject, n))) + 
+  labs(title = "Insolvency by subject", x = "No. of cases", y = "Subject")
+print(insolvency_subject_bar3)
+dev.off()
 
 
 
