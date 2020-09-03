e5b1704d854100675f95c4251a34b6b82584bfc0
eulersim <simone_e@online.de>
2020-09-03 11:52:26 +0200

    Latest version of issue 2

---
+++ code/issue_2_simone.R
@@ -0,0 +1,192 @@
+# ------------------------------------------------------------------------------
+# VHB Course - Group 5
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Issue 2 - Solutions (New Version)
+# ------------------------------------------------------------------------------
+
+### Data preparation ###
+
+# Set working directory
+# Enter your path in the first line and your windows user name in line 13 (replace your name) then the path will be set automatically based on the user
+Paths = c("F:/empirical_accounting_vhb/vhb_qear20", "C:/Users/Simone/Documents/GitHub/vhb_qear20", "C:/Users/tschw/OneDrive/Dokumente/GitHub/vhb_qear20", "C://Olga/other/path")
+names(Paths) = c("pstoczek", "Simone", "tschw", "Olga")
+setwd(Paths[Sys.info()[7]])
+
+#Load required packages
+library(tidyverse)
+library(ggplot2)
+library(summarytools)
+library(sf)
+library(tmap)
+
+#Load insolvency dataset
+insolvency_data <- read.csv("raw_data/insolvency_filings_de_julaug2020_incomplete.csv", 
+                            header = TRUE, 
+                            sep = ",", 
+                            encoding = "UTF-8")
+
+#Load map dataset with geographical data from Germany
+#For downloading the map data, I went to https://gadm.org/download_country_v3.html, searched for "Germany", and saved sf-files in raw_data
+map_federal_states <- readRDS("raw_data/gadm36_DEU_1_sf.rds")
+map_districts <- readRDS("raw_data/gadm36_DEU_2_sf.rds")
+map_municipality <- readRDS("raw_data/gadm36_DEU_3_sf.rds")
+
+prepare_join <- function(map){
+  map$NAME_3[map$NAME_3 == c("Bad Kreuznach(Verbandsgemeinde)","Bad Kreuznach(Verbandsfreie Gemeinde)")] <- "Bad Kreuznach"
+  map$NAME_3[map$NAME_3 == c("Bad Homburg v.d. Höhe")] <- "Bad Homburg v.d.Höhe"
+  map$NAME_3[map$NAME_3 == c("Berlin")] <- "Charlottenburg"
+  map$NAME_3[map$NAME_3 == c("Esslingen am Neckar")] <- "Esslingen"
+  map$NAME_3[map$NAME_3 == c("Frankfurt (Oder)")] <- "Frankfurt/Oder"
+  map$NAME_3[map$NAME_3 == c("Freiburg im Breisgau")] <- "Freiburg"
+  map$NAME_3[map$NAME_3 == c("Kempten (Allgäu)")] <- "Kempten"
+  map$NAME_3[map$NAME_3 == c("Königstein im Taunus")] <- "Königstein/Ts."
+  map$NAME_3[map$NAME_3 == c("Leer (Ostfriesland)")] <- "Leer"
+  map$NAME_3[map$NAME_3 == c("Limburg a.d. Lahn")] <- "Limburg"
+  map$NAME_3[map$NAME_3 == c("Ludwigshafen am Rhein")] <- "Ludwigshafen/Rhein"
+  map$NAME_3[map$NAME_3 == c("Marburg")] <- "Marburg/Lahn"
+  map$NAME_3[map$NAME_3 == c("Mühldorf a. Inn")] <- "Mühldorf"
+  map$NAME_3[map$NAME_3 == c("Neustadt an der Weinstraße")] <- "Neustadt a. d. Wstr."
+  map$NAME_3[map$NAME_3 == c("Weiden i.d. OPf.")] <- "Weiden"
+  map$NAME_3[map$NAME_3 == c("Weilheim i. OB")] <- "Weilheim"
+  map$NAME_3[map$NAME_3 == c("Südtondern")] <- "Niebüll"
+  map$NAME_3[map$NAME_3 == c("Mitteldithmarschen")] <- "Meldorf"
+  map$NAME_3[map$NAME_3 == c("Mittleres Schussental")] <- "Ravensburg"
+  return(map)
+}
+
+map_municipality_clean <- prepare_join(map_municipality)
+
+#Join insolvency data with map data to get additional geographical data
+joined_insolvency_data <- left_join(insolvency_data, map_municipality_clean, by = c("insolvency_court" = "NAME_3")) %>%
+  select(date, insolvency_court, court_file_number, subject, name_debtor, domicile_debtor, NAME_0, NAME_1, NAME_2) %>%
+  rename(country = NAME_0,
+         federal_state = NAME_1,
+         district = NAME_2)
+#Remark: by executing the left_join the number of observations amounts from 10.035 to 10.283 which indicates that there might be a problem.
+#At the moment, I do not know where the problem comes from. Therefore, it should be kept in mind and be subject of further investigations.
+
+#Clean insolvency dataset
+format_variables <- function(data){
+  #transform date from chr to date format
+  data$date <- as.Date(data$date)
+  #transform insolvency_court to factor variable
+  data$insolvency_court <- as.factor(data$insolvency_court)
+  #transform subject to factor variable
+  data$subject <- as.factor(data$subject)
+  #transform country
+  data$country <- as.factor(data$country)
+  #transform federal state
+  data$federal_state <- as.factor(data$federal_state)
+  #transform country
+  data$district <- as.factor(data$district)
+  #remove digits from domicile_debtor as it inconsistent
+  data$domicile_debtor_clean <- as.factor(gsub('[0-9]+', '', data$domicile_debtor))
+  #order columns by name
+  col_order <- c("date", "country", "federal_state", "district", "insolvency_court", "court_file_number", "subject", "name_debtor", "domicile_debtor", "domicile_debtor_clean")
+  data <- data[, col_order]
+  #return dataframe
+  return(data)
+}
+
+joined_insolvency_data_clean <- format_variables(joined_insolvency_data)
+str(joined_insolvency_data_clean)
+
+## Descriptive analysis ###
+
+#Frequency tables
+#Table 1: Frequencies of insolvency filings per insolvency court
+table_1 <- summarytools::freq(joined_insolvency_data_clean$insolvency_court, order = "freq", report.nas = FALSE)
+view(table_1)
+
+#Table 2: Frequencies of insolvency filings per federal state
+table_2 <- summarytools::freq(joined_insolvency_data_clean$federal_state, order = "freq", report.nas = FALSE)
+view(table_2)
+
+#Table 3: Frequencies of insolvency filings per subject
+table_3 <- summarytools::freq(joined_insolvency_data_clean$subject, order = "freq", report.nas = FALSE)
+view(table_3)
+
+#Table 4: Cross-tabulation for pairs of subject and federal states 
+table_4 <- summarytools::ctable(x = joined_insolvency_data_clean$subject, y = joined_insolvency_data_clean$federal_state, prop = "r")
+view(table_4)
+
+#Basic Bar Charts
+#Bar chart 1: shows the insolvency filings per subject
+bar_chart_1 <- ggplot(data = joined_insolvency_data_clean) + 
+  geom_bar(mapping = aes(x = subject, fill = subject))+
+  theme(axis.text.x = element_blank())
+print(bar_chart_1)
+
+#Bar chart 2:shows the insolvency filings by subject
+bar_chart_2  <- joined_insolvency_data_clean %>% 
+  group_by(subject) %>% 
+  count() %>% 
+  arrange(desc(n)) %>%
+  ggplot(aes(x = n, y = reorder(subject, n))) + 
+  geom_col() + 
+  labs(title = "Insolvency filings by subject", x = "No. of filings", y = "Subject")+
+  theme()
+print(bar_chart_2)
+
+# Bar chart 3: shows absolute number of cases by court
+bar_chart_3 <- ggplot(data = joined_insolvency_data_clean, aes(x = insolvency_court, fill = subject)) +
+  geom_bar(col=289)+
+  labs(title = "Cases And Status Of Each Court", x = "Courts" , y = "Number", fill = "Status") +
+  theme(axis.text.x = element_blank()) +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", colour = "black"))
+print(bar_chart_3)
+
+# Bar chart 4: shows development of insolvency subjects over time (TS) 
+Jul.1 <- filter(select(joined_insolvency_data_clean, date:subject), date >= "2020-07-01" & date < "2020-07-15")
+Jul.1$Month <- "(A) July, First Half " 
+Jul.2 <- filter(select(joined_insolvency_data_clean, date:subject), date >= "2020-07-15" & date < "2020-08-01")
+Jul.2$Month <- "(B) July, Second Half" 
+Aug.1 <- filter(select(joined_insolvency_data_clean, date:subject), date >= "2020-08-01" & date < "2020-08-15")
+Aug.1$Month <- "(C) August, First Half" 
+Aug.2 <- filter(select(joined_insolvency_data_clean, date:subject), date >= "2020-08-15" & date < "2020-09-01")
+Aug.2$Month <- "(D) August, Second Half" 
+insolvency_datam <- rbind(Jul.1,Jul.2,Aug.1,Aug.2)
+
+blob:https://web.whatsapp.com/f8d123f7-d3de-45c2-bc5b-4b6f3aaec3ccggplot(data = insolvency_datam, aes(x = subject, fill = subject)) + 
+  geom_bar(col = 289) +
+  facet_grid( ~ Month) +
+  labs(title = "Status by Period", x = "Status" , y = "Number", fill = "Status") +
+  theme(axis.text.x = element_blank()) +
+  theme(plot.title = element_text(hjust = 0.5, face ="bold", colour = "black"))
+
+# Bar chart 5: shows the relative frequencies of insolvency subjects by state
+joined_insolvency_data_clean %>%
+  group_by(federal_state, subject)%>%
+  tally() %>%
+  ungroup() %>%
+  ggplot(aes(x = n, y = federal_state, fill = subject))+
+  geom_bar(position="fill", stat="identity")+
+  xlab("Number of insolvency subjects")+
+  ylab("")+
+  labs(title="Frequency of insolvency subjects by state.")+
+  theme(legend.title = element_blank())
+
+# Interactive Map: shows the insolvency filing for each insolvency_court
+# possibility to filter for specific subjects
+count_insolvency <- joined_insolvency_data_clean  %>%
+  #filter(subject == "Eröffnungen")%>%
+  group_by(insolvency_court)%>%
+  count()
+
+#join datasets
+#map <- left_join(map_municipality_clean, count_insolvency, by = c("NAME_3" = "insolvency_court")) %>%
+#  select(NAME_3, n, geometry)
+
+map <- left_join(count_insolvency, map_municipality_clean, by = c("insolvency_court" = "NAME_3" )) %>%
+  select(insolvency_court, n, geometry)
+
+#transform map
+object <- st_as_sf(map)
+
+#create interactiv map
+tmap_mode("view")
+tm_basemap("OpenStreetMap.DE")+
+  tm_shape(object) +
+  tm_bubbles("n", col = "red")
+
